f8a0d896e5fb4613acaf760b5780dfab824d01a6
Sezim-beep <zholoevasezim@gmail.com>
2025-05-28 11:44:26 +0600

    Добавлена строчка от автора в файл funnyfunnyfunny.py

--- funnyfunnyfunny.py
+++ funnyfunnyfunny.py
@@ -1,1 +1,2 @@
 print("I find studying with guys really fascinating, because of them making up different funny stuff randomly. The girls appear to be really calm and very serious")
+print("The girls are hot, though")
